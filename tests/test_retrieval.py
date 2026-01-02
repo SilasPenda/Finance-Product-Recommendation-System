854f8198e7bd0994067b848059ccd419b2df4e6f
SilasPenda <pendasilas@gmail.com>
2026-01-02 18:08:05 +0100

    First commit

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,10 +1,11 @@
 import os
+import joblib
 import pytest
 import pandas as pd
 from dotenv import load_dotenv
 
 from deployment.inference_pipeline import Inference
-from src.utils import get_config
+from src.utils import get_config, db_client_connect
 
 
 load_dotenv()
@@ -16,22 +17,46 @@ load_dotenv()
 )
 
 @pytest.mark.skipif(
-    not os.getenv("TEST_COLLECTION_NAME"),
+    not os.getenv("COLLECTION_NAME"),
     reason="Contract collection name not set"
 )
 
 def test_qdrant_retrieval():
-    inference = Inference()
+    collection = os.getenv("COLLECTION_NAME")
+    pipeline = joblib.load(os.getenv("DATA_TRANSFORMER"))
 
     config = get_config(os.path.join(os.getcwd(), "config.yaml"))
     feature_columns = config["FEATURE_COLUMNS"]
+
+    client = db_client_connect(collection)
 
     test_data = os.path.join(os.getcwd(), os.getenv("DATA_DIR"), "test_data.csv")
     df = pd.read_csv(test_data)
     df = df.sample(1)
 
-    X = df[feature_columns]
-    results = inference.predict(X)
+    features = df[feature_columns]
+    embeddings = pipeline.transform(features)
+
+    response = client.query_points(
+            collection_name=collection,
+            query=embeddings[0].tolist(),
+            limit=1,
+            with_payload=True
+    )
+
+    points = response.points
+
+
+    results = [
+            {
+                "id": point.id,
+                "score": point.score,
+                "subscribed": point.payload["metadata"]["subscribed"]
+            }
+            for point in points
+        ]
 
     assert results is not None, "No retrieval results returned."
         
+    # Cleanup
+    client.delete_collection(collection_name=collection)
